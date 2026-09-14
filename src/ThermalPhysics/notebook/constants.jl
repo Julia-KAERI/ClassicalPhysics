466933f02dad599acbdd9a03f582418caffe3574
julia-KAERI <julia.kaeri@gmail.com>
2026-09-14 20:02:57 +0900

    20260914_2002

---
+++ src/ThermalPhysics/notebook/constants.jl
@@ -0,0 +1,17 @@
+m_e = 9.109e-31             # electron mass in kg
+m_p = 1.67262192e-27        # proton mass in kg
+c = 299_792_458             # speed of light in m/sec
+J2eV = 6.242e+18            # 1 J = 6.242e+18 eV
+mu = 1.66039e-27            # atomic mass unit in kg
+m_pi = m_e*273              # pi-meson mass
+k_e = 8.987e9               # 1/4πϵ_0 in C^2⋅s^2⋅kg^{−1}⋅m^{−3}
+q_e = 1.602176634e-19       # electron and proton charge in Coulombs.
+ħ = 6.62607015e-34/2/π      # Planck constant in m^2 kg/s
+k_B = 1.380649e-23     # Boltzmann constant in m^2 kg /s^2 /K
+G = 6.674e-11               # 만유인력 상수 in N⋅m^2⋅kg^{-2}
+N_A = 6.02214076e23         # 아보가드로 수
+
+pf2kg = 0.453592            # 1 Pf = 0.453592 kg
+feet2m = 0.3048             # 1 feet = 0.3048 m
+
+;
